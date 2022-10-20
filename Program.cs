274fe18614e32464e922dc036c6b1619d1009dc7
Kevin <kevin.castro.chavez0@gmail.com>
2022-10-20 09:16:38 -0600

    ran migration and created the database scheme. Also, tested CRUD operations

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+// using RazorPagesMovie.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
